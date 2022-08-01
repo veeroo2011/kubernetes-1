4a3a545ea0ac9718ea9dd76c63650c9fdc03b2fa
Abhimnayu kumar <52759159+veeroo2011@users.noreply.github.com>
2022-08-01 16:51:51 +0530

    Update mysql.sql

--- datadump/mysql.sql
+++ datadump/mysql.sql
@@ -1,3 +1,5 @@
 CREATE TABLE test.messages (message VARCHAR(250));
 INSERT INTO test.messages VALUES ('hello');
 INSERT INTO test.messages VALUES ('hey');
+INSERT INTO test.messages VALUES ('Team');
+INSERT INTO test.messages VALUES ('Devops');
